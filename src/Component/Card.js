3698bccb25acd83a84ad1bc4f30cb046a4f3878c
Kamalesh Shaw <kamalesh2110@gmail.com>
2023-01-23 01:14:55 +0530

    frontend create phase 1.0.1

---
+++ src/Component/Card.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Card(props) {
+  console.log(props.data);
+  return <div>Card</div>;
+}
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "./Card";
 
 export default function Main() {
   const [data, setData] = useState({});
@@ -29,6 +30,7 @@ export default function Main() {
       >
         SHOW DATA
       </button>
+      <Card data={data.resp}></Card>
     </div>
   );
 }
