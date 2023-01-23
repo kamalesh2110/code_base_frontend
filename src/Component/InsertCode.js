6c19ebef7b8a01e036d4b247030523b5e778279a
Kamalesh Shaw <kamalesh2110@gmail.com>
2023-01-24 01:28:17 +0530

    1.0.9

--- src/Component/InsertCode.js
+++ src/Component/InsertCode.js
@@ -1,5 +1,27 @@
 import React from "react";
+import "../App.css";
 
 export default function InsertCode() {
-  return <p> insert your code</p>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <input type="text" placeholder="Code Title"></input>
+      <input type="text" placeholder="Code Description"></input>
+      <input type="text" placeholder="Code Author"></input>
+      <input type="text" placeholder="Code Language"></input>
+      <input type="text" placeholder="Code Difficulty"></input>
+      <input type="mail" placeholder="Email"></input>
+      <textarea
+        rows="9"
+        cols="60"
+        name="description"
+        placeholder="enter code"
+      ></textarea>
+    </div>
+  );
 }
